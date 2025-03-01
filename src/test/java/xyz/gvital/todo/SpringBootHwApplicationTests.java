4bb18b1a684eb90e06ad8b1825ba054b3c621253
Vitalii Goncharov <1c.audit@gmail.com>
2025-03-01 11:52:07 +0200

    init app

---
+++ src/test/java/xyz/gvital/todo/SpringBootHwApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.gvital.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
